7891e0f18fbba5cd5e4bbbe6856db61881d79813
KIMDONGWOOK12 <rlaehddnr8838@gmail.com>
2026-05-30 14:25:52 +0900

    3주차 과제 1,2번

--- KTB4-Orbit-AI/과제/2주차/models.py
+++ KTB4-Orbit-AI/과제/2주차/models.py
@@ -1,20 +1,26 @@
+#모델 계층
+# FastAPI 공식 문서
+# URL: https://fastapi.tiangolo.com/tutorial/body/
+# URL : https://fastapi.tiangolo.com/tutorial/body/#create-your-data-model
+
 from typing import Optional
 from pydantic import BaseModel
 
 class PostCreate(BaseModel):
     title: str
-    content: str
+    content : str
 
 class CommentCreate(BaseModel):
-    content: str
+    content : str
+  #  post_id : str
 
 class CommentResponse(BaseModel):
-    id: int
-    post_id: int
-    content: str
+    id : int
+    post_id : int
+    content : str
 
 class PostDetailResponse(BaseModel):
-    id: int
+    id : int
     title: str
-    content: str
-    comments: list[CommentResponse]
+    content : str
+    comments : list[CommentResponse]
